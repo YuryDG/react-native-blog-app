ec0e2e39cec6578fde34e858f5ea3abb1b94ae2b
Yury <yurydgfuentes@gmail.com>
2019-12-29 20:04:22 +0200

    (screen): show post in IndexScreen

--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
+import BlogItem from '../components/BlogItem';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    paddingHorizontal: 10
   },
 });
 
@@ -13,11 +14,15 @@ type IndexScreenProps = {
 }
 
 const IndexScreen: React.FC<IndexScreenProps> = () => {
-  const value = useContext(BlogContext);
+  const { data: blogPosts } = useContext(BlogContext);
 
   return (
     <View style={styles.container}>
-      <Text>Index Screen {value.data}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blog) => String(blog.id)}
+        renderItem={({ item }) => <BlogItem item={item} />}
+      />
     </View>
   );
 };
