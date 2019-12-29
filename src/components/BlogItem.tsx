52abfbec561a525d9f42a03de2108a419fb2d0c7
Yury <yurydgfuentes@gmail.com>
2019-12-29 19:54:38 +0200

    (components): Update styles in BlogItem component

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { BlogPost } from '../interfaces/interfaces';
-
 const styles = StyleSheet.create({
   container: {
-    margin: 5,
-    paddingHorizontal: 5,
-    paddingVertical: 10,
+    paddingVertical: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-
-    // shadow
-    backgroundColor: 'white',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 2,
+    borderBottomColor: '#E9EBEE',
+    borderBottomWidth: 1
   },
 
   title: {
@@ -34,10 +22,7 @@ const styles = StyleSheet.create({
 const BlogItem: React.FC<{ item: BlogPost }> = ({ item }) => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.content}>{item.content}</Text>
-      </View>
+      <Text style={styles.title}>{item.title}</Text>
     </View>
   )
 }
