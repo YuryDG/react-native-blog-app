618e5f24b49819e3327b14384d1a2e54c3269b78
Yury <yurydgfuentes@gmail.com>
2019-12-29 03:41:35 +0200

    (component): Add styles to BlogItem component

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -4,21 +4,40 @@ import { BlogPost } from '../interfaces/interfaces';
 
 const styles = StyleSheet.create({
   container: {
+    margin: 5,
+    paddingHorizontal: 5,
+    paddingVertical: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderTopWidth: 1,
-    borderColor: 'gray'
+
+    // shadow
+    backgroundColor: 'white',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+
+  title: {
+    fontSize: 15
+  },
+  content: {
+    color: 'gray',
   }
 });
 
 const BlogItem: React.FC<{ item: BlogPost }> = ({ item }) => {
   return (
     <View style={styles.container}>
-      <Text>{item.title}</Text>
-      <Text>{item.content}</Text>
+      <View>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.content}>{item.content}</Text>
+      </View>
     </View>
   )
 }
