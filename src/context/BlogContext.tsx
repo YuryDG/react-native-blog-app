60859eb6a9a1138c0452995e22f45f14519334b8
Yury <yurydgfuentes@gmail.com>
2019-12-29 20:03:24 +0200

    (context): add function to add blog to the context and passit to the provider

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BlogPost } from '../interfaces/interfaces'
 
 type ContextType = {
-  data: number;
+  data: BlogPost[],
+  addBlogPost?: Function
 }
 
-// initial value of the context
-const contextInitialValue: ContextType = {
-  data: 10,
+const generateId = () => Math.floor(Math.random() * 9999)
+
+const generateBlogPosts = (): BlogPost[] => {
+  return Array.from({ length: 15 }, (v, k) => {
+    return {
+      id: generateId(),
+      title: `Blog #${k + 1}`,
+      content: `Content for Blog#${k + 1}`
+    }
+  })
 }
 
-const BlogContext = React.createContext<ContextType>({ data: 0 });
+const BlogContext = React.createContext<ContextType>({ data: [] });
 
-// eslint-disable-next-line react/prop-types
 export const BlogProvider: React.FC<{}> = ({ children }) => {
-  return <BlogContext.Provider value={contextInitialValue} >
+
+  const [blogPosts, setBlogPosts] = useState(generateBlogPosts())
+
+  const addBlogPost = () => {
+    const newBlogPost = {
+      id: generateId(),
+      title: `New Blog ${blogPosts.length + 1}`,
+      content: `Content ${blogPosts.length + 1}`,
+    };
+
+    setBlogPosts([...blogPosts, newBlogPost])
+  }
+
+  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }} >
     {children}
   </BlogContext.Provider >
 }
